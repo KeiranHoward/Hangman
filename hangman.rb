25caf7c4e8fe2875ae104950e19591a14739755f
KeiranHoward <64472725+KeiranHoward@users.noreply.github.com>
2020-10-02 21:46:42 +1000

    added word display + theme select

--- hangman.rb
+++ hangman.rb
@@ -8,12 +8,13 @@ require "tty-prompt"
 @logo = "TEST"
 
 class Game
+    attr_accessor :word, :display_word
 
-    def initialize
+    def initialize (word, display_word)
         @alphabet = ("a".."z").to_a
-        @theme = theme
         @word = word
         @attempts = 6
+        @display_word = display_word
     end
 
     def update_screen
@@ -21,48 +22,95 @@ class Game
         puts @logo
     end
 
-    def intro
-        puts "Welcome to Hangman".colorize(:light_blue)
-        puts "Enter your name below".colorize(:light_blue)
-        username = gets.chomp
-    end
-    
+    def user_guess
 
-    def theme
-        @prompt = TTY::Prompt.new
-        @prompt.select("Please select a theme", %w(Animals Geography Careers Computers))
-        case theme_select
-        when %w(Animals)
-            File.open("animals.txt")
-        when %w(Geography)
-            File.open("geography.txt")
-        when %w(Careers)
-            File.open("careers.txt")
-        when %w(Computers)
-            File.open("computers.txt")
-        end
-    end
+        if attempts > 0
+            puts "Enter a letter"
+            guess = gets.chomp
+        end    
+        good_guess = @word.include? guess
 
-    def word
-    
-    end
+        if good_guess == true
+            
+            
+        
+        
+
+    end        
+end  
 
 
-end    
 
 
-@prompt = TTY::Prompt.new
-@prompt.select("Please select a theme", %w(NewGame LoadGame Exit))
+
+####
+## end of class
+
+## game intro
+
+def intro
+    puts "Welcome to Hangman".colorize(:light_blue)
+    puts "Enter your name below".colorize(:light_blue)
+    username = gets.chomp
+end
+
+intro
+system("clear")
+
+##theme select
+
+@prompt.select("Newgame", %w(NewGame LoadGame Exit))
 if %w(NewGame)
-    Game.new
+    puts " "
+
+elsif %w(LoadGame)
+    puts "not yet"    
 else
     puts "not yet"
 end
 
-def load_game
-    
+
+def theme
+    @prompt = TTY::Prompt.new
+    @prompt.select("Please select a theme", %w(Animals Geography Careers Computers))
+    wordlist = nil
+    @word_select = nil
+    case 
+    when %w(Animals)
+        wordlist = File.open("animals.txt")
+        @word_select = wordlist.to_a
+    when %w(Geography)
+        wordlist = File.open("geography.txt")
+        @word_select = wordlist.to_a
+    when %w(Careers)
+        wordlist = File.open("careers.txt")
+        @word_select = wordlist.to_a
+    when %w(Computers)
+        wordlist = File.open("computers.txt")
+        @word_select = wordlist.to_a
+    end
 end
 
-def save_game
+theme
 
+system("clear")
+#loading bar
+
+## pick word and split
+
+def word
+    word = @word_select.map {|word| word.upcase.strip}
+    secretword = word.sample
+    split_word = secretword.chars()
+    display_word = secretword
 end
+
+
+def display_word
+    puts "_ " * word.length
+end
+
+
+start_game = Game.new(word, display_word)
+
+start_game
