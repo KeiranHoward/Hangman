a4736db154454a9196cd7d875a6ad0bd7a17e9c6
KeiranHoward <64472725+KeiranHoward@users.noreply.github.com>
2020-10-02 15:40:36 +1000

    adding prompt gem

--- hangman.rb
+++ hangman.rb
@@ -1,21 +1,18 @@
-require_relative 'animals'
-require_relative 'careers'
-require_relative 'geography'
-require_relative 'computers'
+
 require "rspec"
 require "colorize"
 require "rspec"
 require "tty-prompt"
 
-prompt = TTY::Prompt.new
-@logo = ""
+@prompt = TTY::Prompt.new
+@logo = "TEST"
 
 class Game
 
     def initialize
         @alphabet = ("a".."z").to_a
         @theme = theme
-        @word = words.sample
+        @word = word
         @attempts = 6
     end
 
@@ -28,12 +25,44 @@ class Game
         puts "Welcome to Hangman".colorize(:light_blue)
         puts "Enter your name below".colorize(:light_blue)
         username = gets.chomp
-        puts "/n"
+    end
+    
+
+    def theme
+        @prompt = TTY::Prompt.new
+        @prompt.select("Please select a theme", %w(Animals Geography Careers Computers))
+        case theme_select
+        when %w(Animals)
+            File.open("animals.txt")
+        when %w(Geography)
+            File.open("geography.txt")
+        when %w(Careers)
+            File.open("careers.txt")
+        when %w(Computers)
+            File.open("computers.txt")
+        end
     end
 
-    def menu
-        choices = %w(NewGame LoadGame)
-        prompt.multi_select("Use ↑/↓ arrow keys to select, then hit Enter", choices)
+    def word
+    
     end
 
-    def load_game    
+
+end    
+
+
+@prompt = TTY::Prompt.new
+@prompt.select("Please select a theme", %w(NewGame LoadGame Exit))
+if %w(NewGame)
+    Game.new
+else
+    puts "not yet"
+end
+
+def load_game
+    
+end
+
+def save_game
+
+end
--- tmp.rb
+++ tmp.rb
@@ -1,8 +1,23 @@
+
+require "rspec"
+require "colorize"
+require "rspec"
 require "tty-prompt"
+
 @prompt = TTY::Prompt.new
+@logo = "TEST"
 
-def menu
-    @prompt.select("Choose your destiny?", %w(Scorpion Kano Jax))
+
+class Menu
+
+    def initialize
+        @start = start
+        @prompt = TTY::Prompt.new
+    end
+    
+    def start
+        @prompt = TTY::Prompt.new
+        @prompt.select("Use ↑/↓ arrow keys to select, then hit Enter", %w(NewGame LoadGame))
+
+    end
 end
-
-menu
