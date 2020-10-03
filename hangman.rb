0565f163df5f5ddb8de4178767bad3b6443429ea
KeiranHoward <64472725+KeiranHoward@users.noreply.github.com>
2020-10-03 23:25:42 +1000

    code ajustments

--- hangman.rb
+++ hangman.rb
@@ -11,49 +11,49 @@ require "json"
 
 @prompt = TTY::Prompt.new
 
-puts '                       ___________.._______         '
-puts '                      | .__________))______|        '
-puts '                      | | / /      ||               '
-puts '                      | |/ /       ||               '
-puts '                      | | /        ||.- -.          '
-puts '                      | |/         |/  _  \         '
-puts '                      | |          ||  `/,          '
-puts '                      | |          (\`_.            '
-puts '                      | |         .-`---`.          '
-puts '                      | |        /Y . . Y \          '
-puts '                      | |      / / |   | \ \        '
-puts '                      | |     / /  | . |  \ \        '
-puts '                      | |    (`)   |   |   `(`       '
-puts '                      | |          || ||            '
-puts '                      | |          || ||            '
-puts '                      | |          || ||            '
-puts '                      | |          || ||            '
-puts '                      | |         / | | \           '
-puts '                      |``````````|_`-`` `-`` |```|  '
-puts '                      |`|```````\ \ ````````  |`|   '
-puts '                      | |        \ \          | |   '
-puts '                      : :         \ \         : :   '
-puts '                      . .          `          . .   '
+puts '                       ___________.._______         '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | .__________))______|        '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | | / /      ||               '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |/ /       ||               '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | | /        ||.- -.          '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |/         |/  _  \         '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |          ||  `/,          '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |          (\`_.            '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |         .-`---`.          '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |        /Y . . Y \         '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |      / / |   | \ \        '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |     / /  | . |  \ \       '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |    (`)   |   |   `(`      '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |          || ||            '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |          || ||            '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |          || ||            '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |          || ||            '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |         / | | \           '.colorize(:color => :light_blue, :background => :grey)
+puts '                      |``````````|_`-`` `-`` |```|  '.colorize(:color => :light_blue, :background => :grey)
+puts '                      |`|```````\ \ ````````  |`|   '.colorize(:color => :light_blue, :background => :grey)
+puts '                      | |        \ \          | |   '.colorize(:color => :light_blue, :background => :grey)
+puts '                      : :         \ \         : :   '.colorize(:color => :light_blue, :background => :grey)
+puts '                      . .          `          . .   '.colorize(:color => :light_blue, :background => :grey)
 
 @logo = 
-puts "|--------------------------------------------------------------------------------------------------------------|   "
-puts "|  ooooo   ooooo       .o.       ooooo      ooo   .oooooo.    ooo        ooooo       .o.       ooooo      ooo  |   "
-puts "|  `888    `888       .888.      `888b.     `8   d8P   `Y8b   `88.       .888'      .888.      `888b.     `8   |   "
-puts "|   888     888      .88888.      8 `88b.    8  888            888b     d'888      .88888.      8 `88b.    8   |   "
-puts "|   888ooooo888     .8' `888.     8   `88b.  8  888            8 Y88. .P  888     .8' `888.     8   `88b.  8   |   "  
-puts "|   888     888    .88ooo8888.    8     `88b.8  888     ooooo  8  `888'   888    .88ooo8888.    8     `88b.8   |   "  
-puts "|   888     888   .8'     `888.   8       `888  `88.    .88'   8    Y     888   .8'     `888.   8       `888   |   "
-puts "|  o888o   o888o o88o     o8888o o8o        `8   `Y8bood8P'   o8o        o888o o88o     o8888o o8o        `8   |   "
-puts "|--------------------------------------------------------------------------------------------------------------|   "    
+puts "|--------------------------------------------------------------------------------------------------------------|   ".colorize(:color => :light_blue, :background => :grey)
+puts "|  ooooo   ooooo       .o.       ooooo      ooo   .oooooo.    ooo        ooooo       .o.       ooooo      ooo  |   ".colorize(:color => :light_blue, :background => :grey)
+puts "|  `888    `888       .888.      `888b.     `8   d8P   `Y8b   `88.       .888'      .888.      `888b.     `8   |   ".colorize(:color => :light_blue, :background => :grey)
+puts "|   888     888      .88888.      8 `88b.    8  888            888b     d'888      .88888.      8 `88b.    8   |   ".colorize(:color => :light_blue, :background => :grey)
+puts "|   888ooooo888     .8' `888.     8   `88b.  8  888            8 Y88. .P  888     .8' `888.     8   `88b.  8   |   ".colorize(:color => :light_blue, :background => :grey)  
+puts "|   888     888    .88ooo8888.    8     `88b.8  888     ooooo  8  `888'   888    .88ooo8888.    8     `88b.8   |   ".colorize(:color => :light_blue, :background => :grey)  
+puts "|   888     888   .8'     `888.   8       `888  `88.    .88'   8    Y     888   .8'     `888.   8       `888   |   ".colorize(:color => :light_blue, :background => :grey)
+puts "|  o888o   o888o o88o     o8888o o8o        `8   `Y8bood8P'   o8o        o888o o88o     o8888o o8o        `8   |   ".colorize(:color => :light_blue, :background => :grey)
+puts "|--------------------------------------------------------------------------------------------------------------|   ".colorize(:color => :light_blue, :background => :grey)    
 puts " "
 puts " "
-puts " "
+
 
 @logo                                                                 
                                                                             
 def clear_screen
-    system("clear")
-    @logo
+    return system("clear")
+    return @logo
 end                                                                            
 
 
@@ -66,19 +66,37 @@ class Game
         @word = word
         @display_word = "-" * @word.length
         @used_letters = []
+        @prompt = TTY::Prompt.new
     end
 
     def begin
         while @attempts < 6
             winner = false
+            puts " "
             puts @display_word
+            puts " "
             puts "Enter guess"
             guess = gets.chomp
             update_display(guess) if guess
             winner = winner?
+            loser = loser?
             break if guess == "exit"
             break if winner
+            break if loser
         end 
+    end
+
+    def loser?
+        if @attempts == 6
+            "You lost"
+            @prompt.select("Would you like to play again?", %w(Play Exit))
+            if %w(Play)
+                main_menu
+            elsif %w(Exit)
+                SystemExit
+            end
+            true
+        end  
     end
 
     def winner?
@@ -89,8 +107,8 @@ class Game
             @prompt.select("Would you like to play again?", %w(Play Exit))
             if %w(Play)
                 main_menu
-            else
-                exit
+            elsif %w(Exit)
+                SystemExit
             end
             true
         end        
@@ -109,7 +127,7 @@ class Game
         end
         
         if display_update == @display_word
-            system("clear")
+            clear_screen
             hangman(1)
         else
             hangman(0)
@@ -124,85 +142,85 @@ class Game
         when 0
             puts " "
         when 1
-            puts"    ________   "
-            puts"   |/      |   "
-            puts"   |           "
-            puts'   |           '
-            puts"   |           "
-            puts'   |           '
-            puts"   |           "
-            puts"___|___        "
+            puts"    ________   ".colorize(:light_blue)
+            puts"   |/      |   ".colorize(:light_blue)
+            puts"   |           ".colorize(:light_blue)
+            puts'   |           '.colorize(:light_blue)
+            puts"   |           ".colorize(:light_blue)
+            puts'   |           '.colorize(:light_blue)
+            puts"   |           ".colorize(:light_blue)
+            puts"___|___        ".colorize(:light_blue)
             puts " "
             puts "5 attempts remaining"
             puts " "
             puts "Letters Used: #{@used_letters}"
         when 2
-            @clear_screen
-            puts"    ________   "
-            puts"   |/      |   "
-            puts"   |      (_)  "
-            puts'   |           '
-            puts"   |           "
-            puts'   |           '
-            puts"   |           "
-            puts"___|___        "
+            clear_screen
+            puts"    ________   ".colorize(:light_blue)
+            puts"   |/      |   ".colorize(:light_blue)
+            puts"   |      (_)  ".colorize(:light_blue)
+            puts'   |           '.colorize(:light_blue)
+            puts"   |           ".colorize(:light_blue)
+            puts'   |           '.colorize(:light_blue)
+            puts"   |           ".colorize(:light_blue)
+            puts"___|___        ".colorize(:light_blue)
             puts " "
             puts "4 attempts remaining"
             puts " "
             puts "Letters Used: #{@used_letters}"
         when 3
-            @clear_screen
-            puts"    ________   "
-            puts"   |/      |   "
-            puts"   |      (_)  "
-            puts'   |      \|/  '
-            puts"   |           "
-            puts'   |           '
-            puts"   |           "
-            puts"___|___        "
+            clear_screen
+            puts"    ________   ".colorize(:light_blue)
+            puts"   |/      |   ".colorize(:light_blue)
+            puts"   |      (_)  ".colorize(:light_blue)
+            puts'   |      \|/  '.colorize(:light_blue)
+            puts"   |           ".colorize(:light_blue)
+            puts'   |           '.colorize(:light_blue)
+            puts"   |           ".colorize(:light_blue)
+            puts"___|___        ".colorize(:light_blue)
             puts " "
             puts "3 attempts remaining"
             puts " "
             puts "Letters Used: #{@used_letters}"
         when 4
-            @clear_screen
-            puts"    ________   "
-            puts"   |/      |   "
-            puts"   |      (_)  "
-            puts'   |      \|/  '
-            puts"   |       |   "
-            puts'   |           '
-            puts"   |           "
-            puts"___|___        "
+            clear_screen
+            puts"    ________   ".colorize(:light_blue)
+            puts"   |/      |   ".colorize(:light_blue)
+            puts"   |      (_)  ".colorize(:light_blue)
+            puts'   |      \|/  '.colorize(:light_blue)
+            puts"   |       |   ".colorize(:light_blue)
+            puts'   |           '.colorize(:light_blue)
+            puts"   |           ".colorize(:light_blue)
+            puts"___|___        ".colorize(:light_blue)
             puts " "
             puts "2 attempts remaining"
             puts " "
             puts "Letters Used: #{@used_letters}"
         when 5
-            @clear_screen
-            puts"    ________   "
-            puts"   |/      |   "
-            puts"   |      (_)  "
-            puts'   |      \|/  '
-            puts"   |       |   "
-            puts'   |      /    '
-            puts"   |           "
-            puts"___|___        "
+            clear_screen
+            puts"    ________   ".colorize(:light_blue)
+            puts"   |/      |   ".colorize(:light_blue)
+            puts"   |      (_)  ".colorize(:light_blue)
+            puts'   |      \|/  '.colorize(:light_blue)
+            puts"   |       |   ".colorize(:light_blue)
+            puts'   |      /    '.colorize(:light_blue)
+            puts"   |           ".colorize(:light_blue)
+            puts"___|___        ".colorize(:light_blue)
             puts " "
             puts "1 attempts remaining"
             puts " "
             puts "Letters Used: #{@used_letters}"
         when 6
-            @clear_screen
-            puts"    ________   "
-            puts"   |/      |   "
-            puts"   |      (_)  "
-            puts'   |      \|/  '
-            puts"   |       |   "
-            puts'   |      / \  '
-            puts"   |           "
-            puts"___|___        "
-            puts "You Lose"
+            clear_screen
+            puts"    ________   ".colorize(:light_blue)
+            puts"   |/      |   ".colorize(:light_blue)
+            puts"   |      (_)  ".colorize(:light_blue)
+            puts'   |      \|/  '.colorize(:light_blue)
+            puts"   |       |   ".colorize(:light_blue)
+            puts'   |      / \  '.colorize(:light_blue)
+            puts"   |           ".colorize(:light_blue)
+            puts"___|___        ".colorize(:light_blue)
+            puts "You Lose".colorize(:grey)
             puts " "
             puts "Your word was #{@word}"
         end    
@@ -213,9 +231,15 @@ end
 
 
 def intro
-    puts "Welcome to Hangman".colorize(:light_blue)
-    puts "Enter your name below".colorize(:light_blue)
-    username = gets.chomp
+    puts "Welcome to Hangman!".colorize(:aqua)
+    puts " "
+    puts "Game Rules: Type in a letter".colorize(:aqua)
+    puts "for each incorrect guess, part of a hangman will be added".colorize(:aqua)
+    puts "guess the correct word before the hangman is complete".colorize(:aqua)
+    puts "you can exit at anytime by typing in exit".colorize(:aqua)
+    puts " "
+    puts "Press any key to continue...".colorize(:aqua)
+    enter = gets.chomp
 end
 
 intro
@@ -256,8 +280,8 @@ def main_menu
         if File.exist?("savedgame.json")
             Game.load_game  
         end
-    else
-        exit
+    elsif %w(Exit)
+        SystemExit
     end
         
 end  
