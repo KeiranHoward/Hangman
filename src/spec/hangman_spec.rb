7a487023e90692189b2c7b8d70d9abf9f3b87dbb
KeiranHoward <64472725+KeiranHoward@users.noreply.github.com>
2020-10-04 17:29:18 +1100

    added rspec test for theme select

--- src/spec/hangman_spec.rb
+++ src/spec/hangman_spec.rb
@@ -1,7 +1,7 @@
 
 require 'rspec'
 
-## theme select method test
+# theme select method test
 
 describe 'theme_select' do
     describe '#theme_select' do
@@ -15,15 +15,5 @@ end
 
 # Check if guess is correct
 
-describe 'correct_guess' do
-  it 'gets user input and verifies it against a hidden word' do
-    hidden_word = "bird"
-    user_guess = "b"
-    is_true = false
-    if hidden_word.include? user_guess
-      is_true = true
-    end
-    expect(is_true).to eq(true)
-  end
 
 end
