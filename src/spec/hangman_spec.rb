b57605f887afabf348ec91e24589daf2e806b9bc
KeiranHoward <64472725+KeiranHoward@users.noreply.github.com>
2020-10-04 17:29:48 +1100

    added rspec test for correct guess

--- src/spec/hangman_spec.rb
+++ src/spec/hangman_spec.rb
@@ -15,5 +15,16 @@ end
 
 # Check if guess is correct
 
+describe 'correct_guess' do
+  it 'gets user input and verifies it against a hidden word' do
+    hidden_word = "bird"
+    user_guess = "b"
+    is_true = false
+    if hidden_word.include? user_guess
+      is_true = true
+    end
+    expect(is_true).to eq(true)
+  end
+
 
 end
